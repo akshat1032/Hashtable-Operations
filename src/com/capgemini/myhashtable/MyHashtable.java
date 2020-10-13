873c6de0b2b2bd96707436452edb8c631e6449cd
akshat1032 <akshat.30oct@gmail.com>
2020-10-13 18:59:15 +0530

    Added : Welcome message added

---
+++ src/com/capgemini/myhashtable/MyHashtable.java
@@ -0,0 +1,10 @@
+package com.capgemini.myhashtable;
+
+class MyHashtable {
+	
+	public static void main(String[] args) {
+		
+		// Welcome message added
+		System.out.println("Welcome to Hashtable Operations Program");
+	}
+}
